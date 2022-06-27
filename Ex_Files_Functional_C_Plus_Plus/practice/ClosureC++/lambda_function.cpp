31eba17b6e56ab3feed65a9d1c59ba2ca247be88
Tuan Dinh <dinhngoctuan2131997@gmail.com>
2022-06-27 18:29:48 +0300

    27.06

--- Ex_Files_Functional_C_Plus_Plus/practice/ClosureC++/lambda_function.cpp
+++ Ex_Files_Functional_C_Plus_Plus/practice/ClosureC++/lambda_function.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
+
+void acceptFunctionObject(std::function<void (int&)> f, int y) {
+    f(y);
+}
 
 int main(){
     std::vector<int> data_list {1,2,5,6,7,8,9,4};
@@ -44,6 +49,21 @@ int main(){
     std::sort(data_list.begin(), data_list.end(), comparator); // we pass desc function object to sort function
     std::for_each(data_list.begin(), data_list.end(), visualizer); // we passed visualizer function object
     std::cout << std::endl;
+
+
+    
+    auto lambda3 = [](int& x, int& y) {
+        std::cout << "Subtract x to y : " << x-y << std::endl;
+    };
+
+    using namespace std::placeholders;
+    // neu chi de la ham con tro se bi complier error
+    // nen la dung std::bin()
+    auto lambda_bind = std::bind(lambda3, _1, 5); // _1 co nghia la so duoc gan sau do se tru di 5
+    // Muon ro hon hay doc placeholders
+    
+    //acceptFunctionObject(lambda3, 3);  Compile error
+    acceptFunctionObject(lambda_bind, 3); //  3 - 5 = -2
 
     return 0;
 }
